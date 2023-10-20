7bd76d670f4739a7889a3a41449a43a51a4088fe
Jose Ramon Mañes <jose@celestia.org>
2023-10-20 17:13:07 +0200

    feat(torch): add subgroup /api/v1
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- pkg/http/router.go
+++ pkg/http/router.go
@@ -12,30 +12,32 @@ import (
 func Router(r *mux.Router, cfg config.MutualPeersConfig) *mux.Router {
 	r.Use(LogRequest)
 
+	s := r.PathPrefix("/api/v1").Subrouter()
+
 	// get config
-	r.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
+	s.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
 		GetConfig(w, r, cfg)
 	}).Methods("GET")
 
 	// get nodes
-	r.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+	s.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
 		List(w, r, cfg)
 	}).Methods("GET")
 	// get node details by node name
-	r.HandleFunc("/noId/{nodeName}", func(w http.ResponseWriter, r *http.Request) {
+	s.HandleFunc("/noId/{nodeName}", func(w http.ResponseWriter, r *http.Request) {
 		GetNoId(w, r, cfg)
 	}).Methods("GET")
 
 	// generate
-	r.HandleFunc("/gen", func(w http.ResponseWriter, r *http.Request) {
+	s.HandleFunc("/gen", func(w http.ResponseWriter, r *http.Request) {
 		Gen(w, r, cfg)
 	}).Methods("POST")
-	r.HandleFunc("/genAll", func(w http.ResponseWriter, r *http.Request) {
+	s.HandleFunc("/genAll", func(w http.ResponseWriter, r *http.Request) {
 		GenAll(w, r, cfg)
 	}).Methods("POST")
 
 	// metrics
-	r.Handle("/metrics", promhttp.Handler())
+	s.Handle("/metrics", promhttp.Handler())
 
 	return r
 }
