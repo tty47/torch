40dfa14d6d361db68de0a166a4dbaaea4c1ea8a3
Jose Ramon Mañes <jose@celestia.org>
2023-10-23 13:07:03 +0200

    feat(torch): add tests for nodes file
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

---
+++ pkg/nodes/nodes_test.go
@@ -0,0 +1,77 @@
+package nodes
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jrmanes/torch/config"
+)
+
+func TestValidateNode(t *testing.T) {
+	type args struct {
+		n   string
+		cfg config.MutualPeersConfig
+	}
+	cfg := config.MutualPeersConfig{
+		MutualPeers: []*config.MutualPeer{
+			{
+				Peers: []config.Peer{
+					{NodeName: "da-bridge-1"},
+					{NodeName: "da-bridge-2"},
+				},
+			},
+			{
+				Peers: []config.Peer{
+					{NodeName: "da-bridge-3"},
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name  string
+		args  args
+		want  bool
+		want1 config.Peer
+	}{
+		{
+			name: "Case 1: Node exists in config",
+			args: args{
+				n:   "da-bridge-1",
+				cfg: cfg,
+			},
+			want:  true,
+			want1: config.Peer{NodeName: "da-bridge-1"},
+		},
+		{
+			name: "Case 2: Node exists in config",
+			args: args{
+				n:   "da-bridge-2",
+				cfg: cfg,
+			},
+			want:  true,
+			want1: config.Peer{NodeName: "da-bridge-2"},
+		},
+		{
+			name: "Case 1: Node DOES NOT exists in config",
+			args: args{
+				n:   "nonexistent_node",
+				cfg: cfg,
+			},
+			want:  false,
+			want1: config.Peer{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := ValidateNode(tt.args.n, tt.args.cfg)
+			if got != tt.want {
+				t.Errorf("ValidateNode() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("ValidateNode() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
