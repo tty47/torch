abf16eed92d97f6c5c350d4fd343ef3055311184
Jose Ramon Mañes <jose@celestia.org>
2023-10-28 17:22:59 +0200

    feat(torch): update config - resources
    Signed-off-by: Jose Ramon Mañes <jose@celestia.org>

--- pkg/nodes/queue.go
+++ pkg/nodes/queue.go
@@ -15,7 +15,7 @@ var (
 	taskQueue                   = make(chan config.Peer) // taskQueue channel for pending tasks (peers to process later).
 	MaxRetryCount               = 5                      // MaxRetryCount number of retries per node.
 	TickerTime                  = 5 * time.Second        // TickerTime time specified to make a signal.
-	timeoutDurationProcessQueue = 10 * time.Second       // timeoutDurationProcessQueue time specified to make a signal.
+	timeoutDurationProcessQueue = 60 * time.Second       // timeoutDurationProcessQueue time specified to make a signal.
 )
 
 // ProcessTaskQueue processes the pending tasks in the queue the time specified in the const TickerTime.
